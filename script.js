2546b1cc89bd2ea88a5f003480b32827a554a13b
Tanaya Asalkar <145457627+tanayaasalkar1@users.noreply.github.com>
2026-08-31 14:52:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
 //complete this code
-class Person {}
+class Person {
+	constructor(name, age) {
+		this._name = name;
+		this._age = age;
+	}
+	get name() {
+		return this._name;
+	}
+	set age(age) {
+		return this._age;
+	}
+}
 
-class Student extends Person {}
+class Student extends Person {
+	study() {
+		console.log(this._name " is studying")
+	}
+}
 
-class Teacher extends Person {}
+class Teacher extends Person {
+	teach() {
+console.log(this._name " is teaching")}
+}
 
 // Do not change the code below this line
 window.Person = Person;
